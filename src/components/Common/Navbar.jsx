ff3923e7256c8b9eed980ddae5c1e03a4d8771b5
vishnukkrishna <vishnukkrishna1999@gmail.com>
2024-11-26 18:03:26 +0530

    changed the font size on the navbar.jsx file

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-[#001F3F] rounded-xl w-full text-white h-16 flex items-center justify-center px-4 shadow-md">
-      <h1 className="text-lg tracking-wide sm:text-2xl font-bold text-center">
+      <h1 className="text-base tracking-wide sm:text-2xl font-bold text-center">
         Role-Based Access Control (RBAC)
       </h1>
     </div>
